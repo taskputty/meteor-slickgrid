c3b4208965ca23c90c7489ff80a39964995cd3ac
Graeme Pyle <graeme@graemep.me>
2013-12-08 19:28:35 +0200

    Added SG column picker to package - needed by process gridview

--- package.js
+++ package.js
@@ -7,8 +7,10 @@ Package.on_use(function (api) {
   api.add_files([
       'lib/SlickGrid/slick-default-theme.css',
       'lib/SlickGrid/slick.grid.css',
+
       'lib/jquery.event.drag.js',
       'lib/jquery.event.drop.js',
+
       'lib/SlickGrid/slick.core.js',
       'lib/SlickGrid/slick.dataview.js',
       'lib/SlickGrid/slick.editors.js',
@@ -16,6 +18,15 @@ Package.on_use(function (api) {
       'lib/SlickGrid/slick.groupitemmetadataprovider.js',
       'lib/SlickGrid/slick.grid.js',
       'lib/SlickGrid/slick.remotemodel.js',
+
+      'lib/SlickGrid/controls/slick.columnpicker.js',
+      'lib/SlickGrid/controls/slick.columnpicker.css',
+
+      'lib/SlickGrid/plugins/slick.cellselectionmodel.js',
+      'lib/SlickGrid/plugins/slick.cellrangeselector.js',
+      'lib/SlickGrid/plugins/slick.cellcopymanager.js',
+      'lib/SlickGrid/plugins/slick.cellrangedecorator.js',
+
       'lib/SlickGrid/images/header-columns-bg.gif',
       'lib/SlickGrid/images/header-columns-over-bg.gif'
     ]
